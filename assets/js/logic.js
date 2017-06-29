84da7e5f477c23753f5c5d1c00907c860923e8ee
Taylor Caton <taylorcaton@gmail.com>
2017-06-29 11:17:02 -0400

    finished buttons, pictures, ajax, and form add

---
+++ assets/js/logic.js
@@ -0,0 +1,86 @@
+var topics = ["breakdance", "samba", "salsa", "tap dance", "ballet", "modern dance", "foxtrot", "ballroom dance"];
+var pics;
+var currentTopic;
+
+function pageButtons(){
+
+	$("#buttonBox").empty();
+	for (var i = 0; i < topics.length; i++) {
+		$("#buttonBox").append("<button class='btn btn-default round btnList' data-topic='"+topics[i]+"'>" + topics[i] + "</button>");
+	}
+
+}
+
+function getPictures(topic, callback){
+	// Example queryURL for Giphy API
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+topic+"&api_key=5a6b2fcef7a946b89cf7a4f11c1eb68d&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+      pics = response;
+      callback();
+    });
+}
+
+function emptyPictures(){
+	$("#pictureBox").empty();
+}
+
+function placePictures(){
+	for (var i = 0; i < pics.data.length; i++) {
+		
+		var src = pics.data[i].images.fixed_height_still.url;
+		
+		$("#pictureBox").append("<img src='"+
+								src +
+								"' class='img-fluid thumbnail picList' data-num='"+i+"'>");
+	}
+}
+
+function fixSpaces(){
+	var newStr = "";
+	for (var i = 0; i < currentTopic.length; i++) {
+		if(currentTopic.substring(i, i+1) === " "){
+			newStr += "+";
+		}else {
+			newStr += currentTopic.substring(i, i+1);
+		}
+	}
+
+	return newStr;
+}
+
+window.onload = function() {
+	pageButtons();
+}
+
+$( document ).ready(function() {
+	$(document).on("click", ".btnList", function(){
+		
+		currentTopic = $(this).attr("data-topic");
+		currentTopic = fixSpaces();
+		getPictures(currentTopic, function(){
+			emptyPictures();
+			placePictures();
+		});
+	});
+
+	$(document).on("click", ".picList", function(){
+		var arrayNum = $(this).attr("data-num");
+		var newSrc = pics.data[arrayNum].images.fixed_height.url;
+		$(this).attr("src", newSrc);
+	});
+
+	 $('.addBox').keydown(function(event) {
+        if (event.keyCode == 13) {
+            
+            topics.push($(this).val());
+            pageButtons();
+
+         }
+    });
+
+});
